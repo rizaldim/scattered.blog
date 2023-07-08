df32c2c0a521c43363f3cafd71dc1e0312b31249
Rizaldi Martaputra <zal@fastmail.com>
2023-07-09 00:25:01 +0700

    Use src dir as input and rename 11ty config

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("css");
 	return {
 		dir: {
+			input: "src",
 			// ⚠️ These values are both relative to the input directory.
 			includes: "_includes",
 			layouts: "_layouts"
